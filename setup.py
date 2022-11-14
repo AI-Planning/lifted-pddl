d4375ea5d9b6e8706288b830d6f5c73092db2c59
Carlos Núñez Molina <41746312+TheAeryan@users.noreply.github.com>
2022-11-14 16:32:28 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='lifted-pddl',
+      version='1.0.4',
+      description='A lightweight framework for parsing PDDL in lifted form.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/AI-Planning/lifted-pddl',
+      author='Carlos Núñez Molina',
+      author_email='ccaarlos@ugr.es',
+      license='MIT',
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+      ],
+      keywords='automated_planning PDDL parser',
+      install_requires=['tarski'],
+      include_package_data=True,
+      entry_points={
+          'console_scripts': [
+              'lifted_pddl=lifted_pddl.__main__:main'
+          ],
+      }
+      )
