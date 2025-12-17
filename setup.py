cb40b86fc870195b63f9abb72917be3182596c72
Carlos Núñez Molina <ccaarlos@ugr.es>
2025-12-17 19:57:48 +0100

    Bump version from 1.2.6 to 1.2.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='lifted-pddl',
-      version='1.2.6',
+      version='1.2.7',
       description='A lightweight framework for parsing PDDL in lifted form.',
       long_description=long_description,
       long_description_content_type="text/markdown",
